be4271e0e70ef2ed1b3dd58d46d46a457f743abb
srini@gmail.com <bobbychikkala@Bobbys-iMac.local>
2025-10-15 10:43:50 -0400

    subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
